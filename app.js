9df0beab500d2926c34cc82d4cb0e9ae2f04e39f
samigitt <samialouat@icloud.com>
2022-02-21 10:19:55 +0100

    confirm

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env"});
+
+const app = express();
+
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_user,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+db.connect((error) => {
+    if(error){
+        console.log(error);
+    } else{
+        console.log("mysql connected");
+    }
+
+})
+
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.set("view engine", "hbs");
+
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
+
+app.listen(3306,() => {
+    console.log("server started on port: 3306")
+});
